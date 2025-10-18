b9d3072b15d242891169ba66b1d0621109a467e8
jon <jonathan.thermidor@outlook.com>
2025-10-18 21:04:53 +0200

    DTO implementation (??) + test.

--- src/main/java/com/safetynetalerts/api/ApiApplication.java
+++ src/main/java/com/safetynetalerts/api/ApiApplication.java
@@ -1,13 +1,17 @@
 package com.safetynetalerts.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynetalerts.api.dto.POJODTO;
 import com.safetynetalerts.api.model.POJO;
+import com.safetynetalerts.api.model.Person;
+import com.safetynetalerts.api.service.POJOService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
+import java.util.List;
 
 @SpringBootApplication
 public class ApiApplication implements CommandLineRunner {
@@ -22,7 +26,15 @@ public class ApiApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         POJO data = mapper.readValue(new File("src/main/resources/data.json"), POJO.class);
+
         String prettyJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(data);
         System.out.println(prettyJson);
+
+        List<Person> persons = data.getPersons();
+        List<POJODTO> pojodtoList = POJOService.pojodtoList(persons);
+
+        for(POJODTO pojo : pojodtoList){
+            System.out.println(pojo);
         }
     }
+}
---
+++ src/main/java/com/safetynetalerts/api/dto/POJODTO.java
@@ -0,0 +1,16 @@
+package com.safetynetalerts.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class POJODTO {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+
+}
